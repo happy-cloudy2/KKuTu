a7cc25960050361b310cd282d6faa4713514bf00
happy-cloudy2 <54675727+happy-cloudy2@users.noreply.github.com>
2019-10-16 23:21:42 +0900

    디도스/도스 방어 및 아이피 차단 시 사유를 띄워주는 기능 제작
    제작자: 재미끄투 개발부

--- Server/lib/Web/main.js
+++ Server/lib/Web/main.js
@@ -57,10 +57,16 @@ var gameServers = [];
 WebInit.MOBILE_AVAILABLE = [
 	"portal", "main", "kkutu"
 ];
+function getIP(request){
+  return (request.headers['x-forwarded-for'] ||
+                 request.connection.remoteAddress ||
+                 request.socket.remoteAddress ||
+                 request.connection.socket.remoteAddress).split(',')[0];
+};
 
 require("../sub/checkpub");
 
-JLog.info("<< KKuTu Web >>");
+JLog.info("[[ KKuTu Web ]]");
 Server.set('views', __dirname + "/views");
 Server.set('view engine', "pug");
 Server.use(Express.static(__dirname + "/public"));
@@ -97,15 +103,23 @@ Server.use((req, res, next) => {
 		next();
 	}
 });
+Server.use(function(request, response, next){
+	let blockFinder;
+	
+	if( blockFinder = require(__dirname+"/black.json").find(value=> value.ip==getIP(request)) )
+		page(request, response, {
+			"ip": blockFinder["ip"],
+			"reason": blockFinder["reason"]
+		});
+	else next();
+});
 //볕뉘 수정 끝
-/* use this if you want
-
 DDDoS = new DDDoS({
-	maxWeight: 6,
+	maxWeight: 14,
 	checkInterval: 10000,
 	rules: [{
 		regexp: "^/(cf|dict|gwalli)",
-		maxWeight: 20,
+		maxWeight: 21,
 		errorData: "429 Too Many Requests"
 	}, {
 		regexp: ".*",
@@ -115,7 +129,7 @@ DDDoS = new DDDoS({
 DDDoS.rules[0].logFunction = DDDoS.rules[1].logFunction = function(ip, path){
 	JLog.warn(`DoS from IP ${ip} on ${path}`);
 };
-Server.use(DDDoS.express());*/
+Server.use(DDDoS.express()); //DDoS 방지 시스템
 
 WebInit.init(Server, true);
 DB.ready = function(){
